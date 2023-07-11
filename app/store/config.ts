42a9e7bce381664319f64657b5515df33dbac7c6
supercyx3 <102217889@qq.com>
2023-07-11 19:22:52 +0800

    去除模型config

--- app/store/config.ts
+++ app/store/config.ts
@@ -34,7 +34,7 @@ export const DEFAULT_CONFIG = {
   dontShowMaskSplashScreen: false, // dont show splash screen when create chat
   hideBuiltinMasks: false, // dont add builtin masks
 
-  customModels: "gpt-3.5-turbo-openai,gpt-3.5-turbo-16k-openai,gpt-4-poe,gpt-3.5-turbo-poe,sage,claude-instant,claude+,claude-instant-100k,chat-bison-001,text-davinci-003",
+  customModels: "gpt-3.5-turbo-openai,gpt-3.5-turbo-16k-openai,gpt-4-poe,gpt-3.5-turbo-poe,sage,claude-instant,claude+,claude-instant-100k",
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
