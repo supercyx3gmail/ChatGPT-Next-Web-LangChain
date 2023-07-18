2bb05a00e6bf01e325c6b0ddd6afd74b1c841218
supercyx3 <102217889@qq.com>
2023-07-18 18:10:37 +0800

    更改16k

--- app/store/config.ts
+++ app/store/config.ts
@@ -38,7 +38,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-3.5-turbo-16k-openai" as ModelType,
+    model: "gpt-3.5-turbo-16k" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 32000,
