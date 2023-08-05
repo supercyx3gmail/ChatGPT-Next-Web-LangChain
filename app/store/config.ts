52dfb7af3bc65489920840d3f398db3ce867e678
常雲翔 <103975914+supercyx3@users.noreply.github.com>
2023-08-05 21:05:49 +0800

    model: "gpt-3.5-turbo" as ModelType

--- app/store/config.ts
+++ app/store/config.ts
@@ -38,7 +38,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-3.5-turbo-16k" as ModelType,
+    model: "gpt-3.5-turbo" as ModelType,
     temperature: 0.6,
     top_p: 1,
     max_tokens: 32000,
